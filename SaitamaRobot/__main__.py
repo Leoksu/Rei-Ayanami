4bd8566c284c47698cae93abc3d6b2eea47dcb3f
elka㉨ <80426108+cpsoftboy-cell@users.noreply.github.com>
2021-04-13 00:52:48 +0700

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -339,7 +339,7 @@ def get_help(update: Update, context: CallbackContext):
 
     # ONLY send help in PM
     if chat.type != chat.PRIVATE:
-        if len(args) >= 2 and any(args[1].lower() == ㉨ for ㉨ in HELPABLE):
+        if len(args) >= 2 and any(args[1].lower() == x for x in HELPABLE):
             module = args[1].lower()
             update.effective_message.reply_text(
                 f"Hello! Contact me in PM to get help of {module.capitalize()}",
@@ -372,7 +372,7 @@ def get_help(update: Update, context: CallbackContext):
         )
         return
 
-    elif len(args) >= 2 and any(args[1].lower() == ㉨ for ㉨ in HELPABLE):
+    elif len(args) >= 2 and any(args[1].lower() == x for x in HELPABLE):
         module = args[1].lower()
         text = (
             "Here is the available help for the *{}* module:\n Thanks me later".format(
